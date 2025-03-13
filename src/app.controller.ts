f0d8f2b668e3a9cce304878c9013d41f5f79a2d8
«SergeyVepr» <lolssan@icloud.com>
2025-03-13 21:12:31 +0200

    init

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,50 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+    Body, Controller, Delete, Get, Param, Patch, Post, Put, NotFoundException, BadRequestException
+} from '@nestjs/common';
+import { AppService, Todo } from './app.service';
+import { CreateTodoDto } from './create-todo.dto';
 
-@Controller()
+@Controller('/todos')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Post()
+    createTodo(@Body() createTodoDto: CreateTodoDto): Todo {
+        if (!createTodoDto.title || !createTodoDto.description) {
+            throw new BadRequestException('Title and description are required');
+        }
+        return this.appService.createTodo(createTodoDto);
+    }
+
+    @Get()
+    getAllTodos(): Todo[] {
+        return this.appService.getAllTodos();
+    }
+
+    @Put(':id')
+    updateTodo(@Param('id') id: string, @Body() updateData: Partial<CreateTodoDto>): Todo {
+        const result = this.appService.updateTodo(+id, updateData);
+        if (!result) {
+            throw new NotFoundException(`Todo with id ${id} not found`);
+        }
+        return result;
+    }
+
+    @Patch(':id')
+    updateCompleted(@Param('id') id: string, @Body() completed: boolean): Todo {
+        const result = this.appService.updateCompleted(+id, completed);
+        if (!result) {
+            throw new NotFoundException(`Todo with id ${id} not found`);
+        }
+        return result;
+    }
+
+    @Delete(':id')
+    deleteCompleted(@Param('id') id: string): { message: string } {
+        const success = this.appService.removeTodo(+id);
+        if (!success) {
+            throw new NotFoundException(`Todo with id ${id} not found`);
+        }
+        return { message: 'Deleted' };
+    }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
+import {CreateTodoDto} from "./create-todo.dto";
+
+export interface Todo {
+  id: number;
+  title: string;
+  description: string;
+  date: Date;
+  completed: boolean;
+}
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private todos: Todo[] = [];
+  private id: number = 1;
+
+  createTodo(createTodoDto: CreateTodoDto): Todo {
+      const newTodo: Todo = {
+        id: this.id++,
+        title: createTodoDto.title,
+        description: createTodoDto.description,
+        date: createTodoDto.date,
+        completed: false
+      };
+
+      this.todos.push(newTodo)
+    return newTodo;
+  }
+
+  updateTodo(id: number, updateDate: Partial<CreateTodoDto>): Todo | null {
+    const index = this.todos.findIndex((t) => t.id === id);
+    if(index >= 0) {
+     this.todos[index].title = updateDate.title!;
+     this.todos[index].description = updateDate.description!;
+      return this.todos[index];
+    }
+    return null;
+
+  }
+
+  getAllTodos() {
+    return this.todos;
+  }
+
+  updateCompleted(id: number, completed?: boolean) {
+      const index = this.todos.findIndex(t => t.id === id);
+      if(index >= 0) {
+        this.todos[index].completed = !this.todos[index].completed;
+        return this.todos[index];
+      }
+    return null;
+  }
+
+  removeTodo(id: number) {
+      const index =  this.todos.findIndex(t => t.id === id);
+
+      if(index >= 0) {
+        return this.todos.splice(index, 1)[0];
+      }
+      return null;
   }
 }
